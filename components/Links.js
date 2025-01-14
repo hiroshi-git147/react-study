ff0fcf4628c00d026e0bdd7ef6a8c4b6008ab3c9
“hiroshi-git147” <hiroshigit@gmail.com>
2025-01-14 14:39:35 +0900

    Links.jsリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,32 +1,43 @@
 import Image from "next/image";
 import styles from "./Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    className: styles.primary,
+    description: "Deploy now",
+    icon: "/vercel.svg",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    className: styles.secondary,
+    description: "Read our docs",
+  }
+]
+
 export function Links() {
   return (
     <div className={styles.ctas}>
-      <a
-        className={styles.primary}
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          className={styles.logo}
-          src="/vercel.svg"
-          alt="Vercel logomark"
-          width={20}
-          height={20}
-        />
-        Deploy now
-      </a>
-      <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.secondary}
-      >
-        Read our docs
-      </a>
+      {ITEMS.map((item, index) => (
+        <a
+          key={index}
+          href={item.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={item.className}
+        >
+          {item.icon && (
+            <Image
+              className={styles.logo}
+              src={item.icon}
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+          )}
+          {item.description}
+        </a>
+      ))}
     </div>
   );
 }
